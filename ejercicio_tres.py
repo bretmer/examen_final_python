def46015d2f7d5c4d78ac81886084f0e46e34193
bretmer <condorihuamanbrink05@gmail.com>
2024-07-17 10:53:27 -0500

    solucione: ejercicio tres

--- ejercicio_tres.py
+++ ejercicio_tres.py
@@ -4,3 +4,7 @@ ejm:
 entrada: eucalipto
 salida: {e:1,u:1,a:1,i:1,o:1}
 """
+def contar_vocales(texto):
+    return {vocal: texto.lower().count(vocal)for vocal in "aeiou"}
+
+print(contar_vocales("las clases se python son divertidas"))
