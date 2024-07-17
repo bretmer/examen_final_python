0abfeab117516b19f0dab6c1a347c09d255f294c
bretmer <condorihuamanbrink05@gmail.com>
2024-07-17 10:38:43 -0500

    solucione: ejercicio uno

--- ejercicio_uno.py
+++ ejercicio_uno.py
@@ -4,3 +4,19 @@ ejem:
 entrada: [4,7,10,4,1,0]
 salida : {menor:0,mayor:10}
 """
+def min_max(numeros):
+    numero_menor=numeros[0]
+    numero_mayor=numeros[0]
+    for numero in numeros:
+        if numero<numero_menor:
+            numero_menor=numero
+        elif numero>numero_mayor:
+            numero_mayor=numero
+    resultado={
+        "menor":numero_menor,
+        "mayor":numero_mayor
+    }
+    return resultado
+
+numeros=[5,7,9,2,1]
+print(min_max(numeros))
