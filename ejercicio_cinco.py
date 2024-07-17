a6aea0b94e6f9150ec2aaf6aad9ed2e74bfa3200
bretmer <condorihuamanbrink05@gmail.com>
2024-07-17 09:23:53 -0500

    solucione: ejercicio cinco

--- ejercicio_cinco.py
+++ ejercicio_cinco.py
@@ -4,3 +4,13 @@ ejm:
 entrada: ("jose","alvarez","30","APSTI","III")
 salida: {nombre:"jose",apellido:"alvarez",edad:"30",programa_estudio:"APSTI",semestre:"III"}
 """
+def datos_personales(nombre,apellido,edad,programa_estudio,semestre):
+    return {
+        "nombre":nombre,
+        "apellido":apellido,
+        "edad":edad,
+        "programa_estudio":programa_estudio,
+        "semestre":semestre
+    }
+
+print(datos_personales("bretmer","condori",19,"APSTI","III"))
